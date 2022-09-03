682dd12774e10bbb9fa887b1d6e6cbea9b2ce0d1
serhatuluc <ulucs15@itu.edu.tr>
2022-09-04 02:15:19 +0300

    Added Entity Framework  and Created Database

---
+++ HotelListingAPI/Data/HotelListingDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingAPI.Data
+{
+    public class HotelListingDbContext:DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(new Country
+            {
+                Id = 1,
+                Name = "Jamaica",
+                ShortName = "JM"
+            },
+            new Country
+            {
+                Id=2,
+                Name = "Bahamas",
+                ShortName = "BS"
+            });
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountryId = 2,
+                    Rating = 3.8
+                });
+               
+        }
+
+    }
+}
